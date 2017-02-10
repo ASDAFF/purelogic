8fe4fe3c88849631dd3f2a47b6bb0383a9ad0f88
neeil1990 <neeil@mail.ru>
2017-02-10 14:43:02 +0300

    no message

--- bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001486722945';
-$dateexpire = '001486726545';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:2:"s1";a:1:{i:0;a:2:{s:6:"LD_LID";s:2:"s1";s:9:"LD_DOMAIN";s:23:"tpurelogic.prime-ltd.su";}}}}';
-return true;
-?>
