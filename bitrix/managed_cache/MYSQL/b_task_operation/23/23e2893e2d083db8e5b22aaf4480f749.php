7fb4a62aa3691fbce21d35d1fa96565666118fe1
sv@prime-ltd.su <tpurelogic@prime-host.ru>
2017-02-10 13:26:10 +0300

    cache

--- bitrix/managed_cache/MYSQL/b_task_operation/23/23e2893e2d083db8e5b22aaf4480f749.php
+++ bitrix/managed_cache/MYSQL/b_task_operation/23/23e2893e2d083db8e5b22aaf4480f749.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001486712213';
-$dateexpire = '001486715813';
+$datecreate = '001486717285';
+$dateexpire = '001486720885';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:5:"names";a:0:{}s:3:"ids";a:0:{}}}';
 return true;
 ?>
