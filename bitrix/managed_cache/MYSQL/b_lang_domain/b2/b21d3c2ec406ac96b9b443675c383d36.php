f51c349bec50ec0555034125d1c03d7f63ca8be0
sv@prime-ltd.su <purelogic@test.purelogic.ru>
2017-02-16 12:46:05 +0300

    ok

--- bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
+++ bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001487234511';
-$dateexpire = '001487238111';
+$datecreate = '001487238117';
+$dateexpire = '001487241717';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:2:"s1";a:1:{i:0;a:2:{s:6:"LD_LID";s:2:"s1";s:9:"LD_DOMAIN";s:16:"new.purelogic.ru";}}}}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/b_lang_domain/c0/c0a6035229545c96522ff35e2b8c37a0.php
+++ bitrix/managed_cache/MYSQL/b_lang_domain/c0/c0a6035229545c96522ff35e2b8c37a0.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001487234511';
-$dateexpire = '001487238111';
+$datecreate = '001487238117';
+$dateexpire = '001487241717';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:6:"DOMAIN";a:1:{i:0;a:2:{s:3:"LID";s:2:"s1";s:6:"DOMAIN";s:16:"new.purelogic.ru";}}s:3:"LID";a:1:{s:2:"s1";a:1:{i:0;a:2:{s:3:"LID";s:2:"s1";s:6:"DOMAIN";s:16:"new.purelogic.ru";}}}}}';
 return true;
 ?>
