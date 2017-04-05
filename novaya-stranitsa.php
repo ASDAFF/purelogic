eac0a7f24766123ae5067197658aa77ba68736d9
neeil1990 <neeil@mail.ru>
2017-04-05 11:32:41 +0300

    no message

--- novaya-stranitsa.php
+++ novaya-stranitsa.php
@@ -1,13 +1,16 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Новая страница");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "",
-		"PATH" => "/include/home_baner.php"
-	)
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+?>
+
+
+<?
+if(CModule::IncludeModule('iblock')) {
+	$el = new CIBlockElement;
+	$arLoadProductArray = Array(
+		"NAME" => "Элемент"
+	);
+
+	$PRODUCT_ID = 108255;  // изменяем элемент с кодом (ID) 2
+	$res = $el->Update($PRODUCT_ID, $arLoadProductArray);
+}
+?>
