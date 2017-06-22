5ea8ee6352d86613dd062d0cb1b9d5fb8c85d65d
neeil1990 <neeil@mail.ru>
2017-06-22 10:43:21 +0300

    no message

--- bitrix/templates/first_copy/components/bitrix/catalog/new/bitrix/catalog.section/window/template.php
+++ bitrix/templates/first_copy/components/bitrix/catalog/new/bitrix/catalog.section/window/template.php
@@ -70,7 +70,7 @@ foreach ($arResult['ITEMS'] as $arItem) {
 
 				if(empty($arItem['PREVIEW_PICTURE']['SRC'])){$arItem['PREVIEW_PICTURE']['SRC'] = '/img/nophoto.png';} ?>
 				<a href="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" class="image fancybox-one" data-fancybox-group="thumb<?=$arItem['ID']?>">
-					<img style="max-width: 100%;" src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" alt="<?=$arItem['NAME'] ?>">
+					<img style="max-width: 123px;" src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" alt="<?=$arItem['NAME'] ?>">
 				</a>
 
 				<?
